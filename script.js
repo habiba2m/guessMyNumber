65351b6d6102d1fd9b2965fdf83650a73c469586
Habiba Mohamadeen <66543958+habiba2m@users.noreply.github.com>
2022-08-22 21:24:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
-'use strict';
+"use strict";
 
-if (localStorage.getItem('highScore') === null) {
-  localStorage.setItem('highScore', 0);
+if (localStorage.getItem("highScore") === null) {
+  localStorage.setItem("highScore", 0);
 }
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highScore = localStorage.getItem('highScore');
-document.querySelector('.highscore').textContent = highScore;
+let highScore = localStorage.getItem("highScore");
+document.querySelector(".highscore").textContent = highScore;
 
 function checking() {
-  const guess = Number(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 
   //No input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+    document.querySelector(".message").textContent = "⛔ No number!";
 
     //Correct answer
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎊 Correct number!';
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
+    document.querySelector(".message").textContent = "🎊 Correct number!";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
 
     if (score > highScore) {
       highScore = score;
-      highScore.textContent = highScore;
-      localStorage.setItem('highScore', highScore);
+      document.querySelector(".highscore").textContent = highScore;
+      localStorage.setItem("highScore", highScore);
     }
 
     //guess is wrong
   } else if (guess !== secretNumber) {
-    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector("body").style.backgroundColor = "black";
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? '👆 Too high!' : '👇 Too low!';
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "👆 Too high!" : "👇 Too low!";
       score--;
-      document.querySelector('.score').textContent = score;
+      document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector('.message').textContent = '😥 You lost the game';
-      document.querySelector('.score').textContent = 0;
+      document.querySelector(".message").textContent = "😥 You lost the game";
+      document.querySelector(".score").textContent = 0;
     }
   }
 }
 
-document.querySelector('.check').addEventListener('click', checking);
+document.querySelector(".check").addEventListener("click", checking);
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.number').textContent = '?';
+  document.querySelector(".number").textContent = "?";
   score = 20;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = 'black';
-  document.querySelector('.number').style.width = '15rem';
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "black";
+  document.querySelector(".number").style.width = "15rem";
 });
 
-document.addEventListener('keydown', function (event) {
+document.addEventListener("keydown", function (event) {
   console.log(KeyboardEvent);
-  if (event.key === 'Enter') {
+  if (event.key === "Enter") {
     checking();
   }
 });
